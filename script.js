0ba48f69a039cc37bf062ca8f12cea56f3f93792
sampathkumarmahankali <mahankalisampath2@gmail.com>
2023-09-27 22:36:08 +0530

    sam

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function sam(){
     let navMenu = document.querySelector('.onds');
     navMenu.classList.toggle("visible");
 }
+const signin=document.querySelector(".signin");
+const signup=document.querySelector(".signup");
+const clickme=document.querySelector(".clickhere");
+const log=document.querySelector(".start");
+function login(){
+    signin.classList.remove("disable");
+    log.classList.add("disable");
+}
+function change(){
+     signin.classList.add("disable");
+     signup.classList.remove("disable")
+}
