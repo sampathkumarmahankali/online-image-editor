59841b85c7d3569a7e43025af5c40075ff44f308
sampathkumarmahankali <mahankalisampath2@gmail.com>
2023-10-05 10:46:04 +0530

    sam

--- filters.js
+++ filters.js
@@ -8,6 +8,7 @@ const file_input=document.querySelector(".file-input");
 const option=document.querySelectorAll(".option");
 const save=document.querySelector(".save");
 
+let bright=1;
 let grayscale=0;
 let blured=0;
 let contrast=100;
@@ -64,7 +65,7 @@ const saveimg=()=>{
     const ctx=canvas.getContext("2d");
     canvas.width=preview_img.naturalWidth;
     canvas.height=preview_img.naturalHeight;
-    ctx.filter=`grayscale(${grayscale}%) blur(${blured}px) contrast(${contrast}%) sepia(${sepia}%)`;
+    ctx.filter=`grayscale(${grayscale}%) blur(${blured}px) contrast(${contrast}%) sepia(${sepia}%) brightness(${bright})`;
     ctx.drawImage(preview_img,0,0,canvas.width,canvas.height);
     let link=document.createElement("a");
     link.href=canvas.toDataURL();
@@ -74,3 +75,69 @@ const saveimg=()=>{
 file_input.addEventListener("change",loadimage);
 save.addEventListener("click",saveimg);
 choose.addEventListener("click",()=>{file_input.click()});
+        let recognition = null;
+
+        function startRecognition() {
+            if (recognition) {
+                recognition.stop();
+                recognition = null;
+            }
+
+            recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+            recognition.continuous = true;
+            recognition.lang = 'en-US';
+
+            recognition.onstart = () => {
+                console.log('Speech recognition started');
+                document.getElementById('startRecognition').textContent = 'Stop voice filtering';
+                document.getElementById('startRecognition').classList.add("actived");
+            };
+
+            recognition.onend = () => {
+                recognition.stop();
+                console.log('Speech recognition stopped');
+                document.getElementById('startRecognition').textContent = 'Start voice filtering';
+                document.getElementById('startRecognition').classList.remove("actived");
+            };
+
+            recognition.onresult = (event) => {
+                const result = event.results[event.results.length - 1][0].transcript.toLowerCase(); // Only the first word
+                console.log('Recognized: ' + result);
+
+                if (result.includes('brighten')) {
+                    adjustBrightness(1.2);
+                } else if (result.includes('darken')) {
+                    adjustBrightness(0.8);
+                }else if (result.includes('download')) {
+                    saveimg();
+                }else if(result.includes('black and white')){
+                    preview_img.style.filter=`grayscale(100%)`;
+                    grayscale=100;
+                }
+            };
+
+            recognition.start();
+        }
+
+        function stopRecognition() {
+            if (recognition) {
+                recognition.stop();
+                recognition = null;
+                document.getElementById('startRecognition').textContent = 'Start Speech Recognition';
+            }else{
+                console.log('Recognition was not running.');
+            }
+        }
+
+        function adjustBrightness(factor) {
+            preview_img.style.filter=`brightness(${factor})`;
+            bright=factor;
+        }
+
+        document.getElementById('startRecognition').addEventListener('click', () => {
+            if (recognition && recognition.running) {
+                stopRecognition();
+            } else {
+                startRecognition();
+            }
+        });
